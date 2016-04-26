799fb0d8fc7c65547357f89a1de963c10c58648b
Loeing <jlt81@case.edu>
2016-04-26 07:39:20 -0400

    android hell

---
+++ src/server/pipeline/DPPTAM/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/josh/Workspace/unkei/src/server/pipeline/DPPTAM/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/josh/Workspace/unkei/src/server/pipeline/DPPTAM/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/josh/Workspace/unkei/src/server/pipeline/DPPTAM/devel/lib:/home/josh/Workspace/unkei/src/server/pipeline/DPPTAM/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/josh/Workspace/unkei/src/server/pipeline/DPPTAM/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/josh/Workspace/unkei/src/server/pipeline/DPPTAM/devel/lib/pkgconfig:/home/josh/Workspace/unkei/src/server/pipeline/DPPTAM/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PYTHONPATH="/home/josh/Workspace/unkei/src/server/pipeline/DPPTAM/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/josh/Workspace/unkei/src/server/pipeline/DPPTAM/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/josh/Workspace/unkei/src/server/pipeline/DPPTAM:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
